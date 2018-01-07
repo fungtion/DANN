691672b90bf6071776b522bca2febba8b50edf90
wfx <fungtion.wong@gmail.com>
2018-01-07 18:13:11 +0800

    first commit

---
+++ train/main.py
@@ -0,0 +1,161 @@
+import random
+import os
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
+from dataset.data_loader import GetLoader
+from torchvision import datasets
+from torchvision import transforms
+from models.model import CNNModel
+import numpy as np
+from test import test
+
+source_dataset_name = 'mnist'
+target_dataset_name = 'mnist_m'
+source_image_root = os.path.join('..', 'dataset', source_dataset_name)
+target_image_root = os.path.join('..', 'dataset', target_dataset_name)
+model_root = os.path.join('..', 'models')
+cuda = True
+cudnn.benchmark = True
+lr = 1e-3
+batch_size = 128
+image_size = 28
+n_epoch = 100
+
+manual_seed = random.randint(1, 10000)
+random.seed(manual_seed)
+torch.manual_seed(manual_seed)
+
+# load data
+
+img_transform = transforms.Compose([
+    transforms.RandomResizedCrop(image_size),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+])
+
+dataset_source = datasets.MNIST(
+    root=source_image_root,
+    train=True,
+    transform=img_transform,
+)
+
+dataloader_source = torch.utils.data.DataLoader(
+    dataset=dataset_source,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=8)
+
+train_list = os.path.join(target_image_root, 'mnist_m_train_labels.txt')
+
+dataset_target = GetLoader(
+    data_root=os.path.join(target_image_root, 'mnist_m_train'),
+    data_list=train_list,
+    transform=img_transform
+)
+
+dataloader_target = torch.utils.data.DataLoader(
+    dataset=dataset_target,
+    batch_size=batch_size,
+    shuffle=True,
+    num_workers=8)
+
+# load model
+
+my_net = CNNModel()
+
+# setup optimizer
+
+optimizer = optim.Adam(my_net.parameters(), lr=lr)
+
+loss_class = torch.nn.NLLLoss()
+loss_domain = torch.nn.NLLLoss()
+
+if cuda:
+    my_net = my_net.cuda()
+    loss_class = loss_class.cuda()
+    loss_domain = loss_domain.cuda()
+
+for p in my_net.parameters():
+    p.requires_grad = True
+
+# training
+
+for epoch in xrange(n_epoch):
+
+    len_dataloader = min(len(dataloader_source), len(dataloader_target))
+    data_source_iter = iter(dataloader_source)
+    data_target_iter = iter(dataloader_target)
+
+    i = 0
+    while i < len_dataloader:
+
+        p = float(i + epoch * len_dataloader) / n_epoch / len_dataloader
+        alpha = 2. / (1. + np.exp(-10 * p)) - 1
+
+        # training model using source data
+        data_source = data_source_iter.next()
+        s_img, s_label = data_source
+
+        my_net.zero_grad()
+        batch_size = len(s_label)
+
+        input_img = torch.FloatTensor(batch_size, 3, image_size, image_size)
+        class_label = torch.LongTensor(batch_size)
+        domain_label = torch.zeros(batch_size)
+        domain_label = domain_label.long()
+
+        if cuda:
+            s_img = s_img.cuda()
+            s_label = s_label.cuda()
+            input_img = input_img.cuda()
+            class_label = class_label.cuda()
+            domain_label = domain_label.cuda()
+
+        input_img.resize_as_(s_img).copy_(s_img)
+        class_label.resize_as_(s_label).copy_(s_label)
+        inputv_img = Variable(input_img)
+        classv_label = Variable(class_label)
+        domainv_label = Variable(domain_label)
+
+        class_output, domain_output = my_net(input_data=inputv_img, alpha=alpha)
+        err_s_label = loss_class(class_output, classv_label)
+        err_s_domain = loss_domain(domain_output, domainv_label)
+
+        # training model using target data
+        data_target = data_target_iter.next()
+        t_img, _ = data_target
+
+        batch_size = len(t_img)
+
+        input_img = torch.FloatTensor(batch_size, 3, image_size, image_size)
+        domain_label = torch.ones(batch_size)
+        domain_label = domain_label.long()
+
+        if cuda:
+            t_img = t_img.cuda()
+            input_img = input_img.cuda()
+            domain_label = domain_label.cuda()
+
+        input_img.resize_as_(t_img).copy_(t_img)
+        inputv_img = Variable(input_img)
+        domainv_label = Variable(domain_label)
+
+        _, domain_output = my_net(input_data=inputv_img, alpha=alpha)
+        err_t_domain = loss_domain(domain_output, domainv_label)
+        err = err_t_domain + err_s_domain + err_s_label
+        err.backward()
+        optimizer.step()
+
+        i += 1
+
+        print 'epoch: %d, [iter: %d / all %d], err_s_label: %f, err_s_domain: %f, err_t_domain: %f' \
+              % (epoch, i, len_dataloader, err_s_label.cpu().data.numpy(),
+                 err_s_domain.cpu().data.numpy(), err_t_domain.cpu().data.numpy())
+
+    torch.save(my_net, '{0}/mnist_mnistm_model_epoch_{1}.pth'.format(model_root, epoch))
+    test(source_dataset_name, epoch)
+    test(target_dataset_name, epoch)
+
+print 'done'
---
+++ train/test.py
@@ -0,0 +1,100 @@
+import os
+import torch.backends.cudnn as cudnn
+import torch.utils.data
+from torch.autograd import Variable
+from torchvision import transforms
+from dataset.data_loader import GetLoader
+from torchvision import datasets
+
+
+def test(dataset_name, epoch):
+    assert dataset_name in ['mnist', 'mnist_m']
+
+    model_root = os.path.join('..', 'models')
+    image_root = os.path.join('..', 'dataset', dataset_name)
+
+    cuda = True
+    cudnn.benchmark = True
+    batch_size = 128
+    image_size = 28
+    alpha = 0
+
+    """load data"""
+
+    img_transform = transforms.Compose([
+        transforms.RandomResizedCrop(image_size),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+    ])
+
+    if dataset_name == 'mnist_m':
+        test_list = os.path.join(image_root, 'mnist_m_test_labels.txt')
+
+        dataset = GetLoader(
+            data_root=os.path.join(image_root, 'mnist_m_test'),
+            data_list=test_list,
+            transform=img_transform
+        )
+    else:
+        dataset = datasets.MNIST(
+            root=image_root,
+            train=False,
+            transform=img_transform,
+        )
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset=dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=8
+    )
+
+    """ training """
+
+    my_net = torch.load(os.path.join(
+        model_root, 'mnist_mnistm_model_epoch_' + str(epoch) + '.pth'
+    ))
+    my_net = my_net.eval()
+
+    if cuda:
+        my_net = my_net.cuda()
+
+    len_dataloader = len(dataloader)
+    data_target_iter = iter(dataloader)
+
+    i = 0
+    n_total = 0
+    n_correct = 0
+
+    while i < len_dataloader:
+
+        # test model using target data
+        data_target = data_target_iter.next()
+        t_img, t_label = data_target
+
+        batch_size = len(t_label)
+
+        input_img = torch.FloatTensor(batch_size, 3, image_size, image_size)
+        class_label = torch.LongTensor(batch_size)
+
+        if cuda:
+            t_img = t_img.cuda()
+            t_label = t_label.cuda()
+            input_img = input_img.cuda()
+            class_label = class_label.cuda()
+
+        input_img.resize_as_(t_img).copy_(t_img)
+        class_label.resize_as_(t_label).copy_(t_label)
+        inputv_img = Variable(input_img)
+        classv_label = Variable(class_label)
+
+        class_output, _ = my_net(input_data=inputv_img, alpha=alpha)
+        pred = class_output.data.max(1, keepdim=True)[1]
+        n_correct += pred.eq(classv_label.data.view_as(pred)).cpu().sum()
+        n_total += batch_size
+
+        i += 1
+
+        accu = n_correct * 1.0 / n_total
+
+    print 'epoch: %d, accuracy of the %s dataset: %f' % (epoch, dataset_name, accu)
